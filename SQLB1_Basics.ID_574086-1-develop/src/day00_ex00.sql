7f756bf4d047cd0fce39c000564ffec4630b20af
EATSLEEPSESH <cloudsasjury@gmail.com>
2025-12-15 10:50:22 +0300

    labs

--- SQLB1_Basics.ID_574086-1-develop/src/day00_ex00.sql
+++
@@ -1,3 +0,0 @@
--- Active: 1760733443343@@127.0.0.1@5432@postgres@public
-SELECT * FROM person 
-WHERE address = 'Kazan';
--- SQLB1_Basics.ID_574086-1-develop/src/day00_ex05.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-    (SELECT name FROM person WHERE id = po.person_id) AS NAME
-FROM person_order po
-WHERE (menu_id = 13 or menu_id = 14 or menu_id = 18)
-  AND order_date = '2022-01-07';
